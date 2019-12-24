aa889837d5c2c24fa3089dc67bbb2276e706be17
Ashif Zafar <ashif.mohammad@cdk.com>
2019-12-24 16:51:27 +0530

    first commit

---
+++ src/components/Home.js
@@ -0,0 +1,7 @@
+import React from "react"
+import {VegStoreHeader} from "../materialComponents";
+
+
+export const Home = () => <div><VegStoreHeader>
+    <div>This is the home page</div>
+</VegStoreHeader></div>
---
+++ src/materialComponents/AppBar.js
@@ -0,0 +1,26 @@
+import React from "react";
+import {
+  AppBar,
+  Toolbar,
+  Button,
+  IconButton,
+  Typography
+} from "@material-ui/core";
+import MenuIcon from "@material-ui/icons/Menu";
+import "./style.css";
+
+export const VegStoreHeader = () => {
+  return (
+    <AppBar className="app-bar-main">
+      <Toolbar>
+        <IconButton edge="start" color="inherit" aria-label="menu">
+          <MenuIcon />
+        </IconButton>
+        <Typography variant="h6">Order Vegetables Online !!!</Typography>
+        <Button color="inherit" className="app-bar-login-btn">
+          Login
+        </Button>
+      </Toolbar>
+    </AppBar>
+  );
+};
